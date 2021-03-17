144e64ee16334a6b506b9198dbfa53612c1b3983
cmatii <matias.vargas.marin@gmail.com>
2021-03-16 21:45:04 -0300

    Incializacion del proyecto
    proyecto de clase

---
+++ Clase 2/Assets/Scritps/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+ public float Speed = 1.0f;
+  public float RotationSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible= false;
+
+
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, 0.0f, vertical)*Time.deltaTime*Speed);
+
+        float rotationY = Input.GetAxis("Mouse X");
+        
+        transform.Rotate(new Vector3(0, rotationY*Time.deltaTime*RotationSpeed ,0 ));
+
+
+    }
+}
